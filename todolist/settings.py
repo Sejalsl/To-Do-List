ed118460f09cdb82f877760fcf5a8cc3893b2a37
Sejalsl <slsejal22@gmail.com>
2025-12-22 10:33:35 +0530

    fix environment setup, migrations, and app working

--- todolist/settings.py
+++ todolist/settings.py
@@ -10,10 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import environ
+import os          
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
